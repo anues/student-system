89d8d7ef5a2e29ee4a1fef4401f9d76e1541da3b
anues <134240260+anues@users.noreply.github.com>
2026-09-18 21:38:43 +0200

    Update db.php

--- db.php
+++ db.php
@@ -7,7 +7,7 @@ ini_set('display_errors', 1);
 $host    = 'sql10.freesqldatabase.com';
 $dbname  = 'sql10837288';
 $user    = 'sql10837288';
-$pass    = '1711567'; 
+$pass    = 'anisanues203310'; 
 
 try {
     // إنشاء الاتصال باستخدام PDO
